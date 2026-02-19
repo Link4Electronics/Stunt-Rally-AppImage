962526349617903c6a192c2d049f7e6b662ebfc8
Link4Electronics <link4electronics@gmail.com>
2026-02-19 20:12:27 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,22 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    boost      \
+    bullet     \
+    enet       \
+    mygui-ogre \
+    openal     \
+    sdl2       \
+    tinyxml    \
+    tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package ogre-legacy
 make-aur-package stuntrally-git
 
 # If the application needs to be manually built that has to be done down here
