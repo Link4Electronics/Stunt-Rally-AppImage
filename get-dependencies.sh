61966a22e679e20e33c16c07fdedf8f64e604cf1
Link4Electronics <link4electronics@gmail.com>
2026-02-20 00:41:29 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,7 +11,7 @@ pacman -Syu --noconfirm \
     bullet              \
     clang               \
     cmake               \
-    foxygen             \
+    doxygen             \
     enet                \
     hicolor-icon-theme  \
     libdecor            \
