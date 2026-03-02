fddab59c57449ce7e21aba6b5b0c3f85db9045a9
Link4Electronics <link4electronics@gmail.com>
2026-03-02 03:34:39 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -27,17 +27,25 @@ get-debloated-pkgs --add-common --prefer-nano ! llvm
 #make-aur-package ogre-next2
 
 # If the application needs to be manually built that has to be done down here
-echo "Making nightly build of Stunt Rally..."
-echo "---------------------------------------------------------------"
-REPO="https://github.com/stuntrally/stuntrally3"
-VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./stuntrally
-echo "$VERSION" > ~/version
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of Stunt Rally..."
+    echo "---------------------------------------------------------------"
+    REPO="https://github.com/stuntrally/stuntrally3"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone "$REPO" ./stuntrally
+    echo "$VERSION" > ~/version
 
-cd ./stuntrally
-python3 build-sr3-Linux.py
-mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j$(nproc)
-mv -v stuntrally3 ../../AppDir/bin
-mv -v dist/stuntrally3.desktop ../../AppDir
+    mkdir -p ./AppDir/bin
+    cd ./stuntrally
+    python3 build-sr3-Linux.py
+    mkdir -p build && cd build
+    cmake .. -DCMAKE_BUILD_TYPE=Release
+    make -j$(nproc)
+    mv -v stuntrally3 ../../AppDir/bin
+    mv -v dist/stuntrally3.desktop ../../AppDir
+else
+    mkdir -p ./AppDir/bin
+    wget https://downloads.sourceforge.net/project/stuntrally/files/3.3/StuntRally-3.3-Linux.txz
+    bsdtar -xvf StuntRally-3.3-Linux.txz
+    mv -v StuntRally-3.3-Linux ./AppDir/bin/stuntrally3
+fi
