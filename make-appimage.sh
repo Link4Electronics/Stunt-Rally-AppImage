b874425af04ec147c8a19936c43bd1afa5e1f779
Link4Electronics <link4electronics@gmail.com>
2026-03-02 04:39:07 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export STARTUPWMCLASS=
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/Release/stuntrally3
+quick-sharun ./AppDir/bin/bin/Release/stuntrally3
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
