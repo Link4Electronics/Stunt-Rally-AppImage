160c1d286f602378346fd7f18a7e0a3d338a1ba7
Link4Electronics <link4electronics@gmail.com>
2026-03-13 05:11:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,10 +18,10 @@ pacman -Syu --noconfirm libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ! llvm
+get-debloated-pkgs --add-common --prefer-nano #! llvm
 
 # Comment this out if you need an AUR package
-make-aur-package boost174-libs
+#make-aur-package boost174-libs
 
 # If the application needs to be manually built that has to be done down here
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
